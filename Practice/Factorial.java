c762c5bec3e2b7d592458c198bce61172ff85358
raji119 <sudhakarraji46@gmail.com>
2023-08-26 21:11:53 +0530

    Fcatorial

---
+++ Practice/Factorial.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Factorial {
+   public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int n;
+    System.out.println("Enter n:");
+    n=sc.nextInt();
+
+    int fact=1;
+    if(n>=0){
+        for(int i=2;i<=n;i++){
+            fact*=i;
+        }
+        System.out.println("Fact= "+fact);
+    }
+    else{
+        System.out.println("Factorial is Not possible");
+    }
+   } 
+}
