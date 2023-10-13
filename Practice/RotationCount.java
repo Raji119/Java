fe9d05b8c67e936db9bdff091cbe9b3a757da0ad
raji119 <sudhakarraji46@gmail.com>
2023-10-13 09:00:51 +0530

    Rotation Count

---
+++ Practice/RotationCount.java
@@ -0,0 +1,39 @@
+public class RotationCount {
+    
+        public static void main(String[] args) {
+            int[] num={5,6,7,1,2,3};
+            int index=search(num);
+            System.out.println(index);
+    
+        }
+    
+            public  static int search(int[] nums) {
+        
+                int pivot = findPivot(nums);
+                return pivot+1;
+            }
+        
+            public static int findPivot(int[] nums){
+                int start =0;
+                int end=nums.length-1;
+        
+                while(start<=end){
+                    int mid=start+(end-start)/2;
+                    if(mid<end && nums[mid]>nums[mid+1]){
+                        return mid;
+                    }
+                    if(mid>start && nums[mid]<nums[mid-1]){
+                        return mid-1;
+                    }
+                    if(nums[mid]<=nums[start]){
+                        end=mid-1;
+                    }else{
+                        start=mid+1;
+                    }
+                }
+                return 0;
+            }
+        
+    }
+    
+    
