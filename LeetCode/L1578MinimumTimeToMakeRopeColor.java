7180b6ace58293dbd47e3aec23935706480eb89a
raji119 <sudhakarraji46@gmail.com>
2023-12-27 19:30:33 +0530

    Minimum Time To Make Rope Color

---
+++ LeetCode/L1578MinimumTimeToMakeRopeColor.java
@@ -0,0 +1,17 @@
+package LeetCode;
+
+public class L1578MinimumTimeToMakeRopeColor {
+    public int minCost(String colors, int[] neededTime) {
+
+        int time = 0;
+        int n = colors.length();
+        for (int i = 1; i < n; i++) {
+            if (colors.charAt(i) == colors.charAt(i - 1)) {
+                time += Math.min(neededTime[i], neededTime[i - 1]);
+                neededTime[i] = Math.max(neededTime[i], neededTime[i - 1]);
+            }
+        }
+        return time;
+
+    }
+}
